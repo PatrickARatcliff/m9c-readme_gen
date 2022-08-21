ca37ac248f68c558fe6717ea58ee8dde1543a5af
PatrickARatcliff <pratcliff5@gmail.com>
2022-08-21 14:53:17 -0600

    npi init/i, require fs, inquirer, genMD, add q's

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs')
+const genMD = require("../m9c-readme_gen/utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'title',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'description',
+        message: 'Enter a description for your project:',
+        name: 'location',
+    },
+    {
+        type: 'install',
+        message: 'Enter instructions for installation:',
+        name: 'install',
+    },
+    {
+        type: 'usage',
+        message: 'Enter usage information:',
+        name: 'usage',
+    },
+    {
+        type: 'contribute',
+        message: 'Enter contribution guidelines:',
+        name: 'contribute',
+    },
+    {
+        type: 'test',
+        message: 'Enter test instructions:',
+        name: 'test',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
